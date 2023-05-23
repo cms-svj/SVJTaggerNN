fixed bugs with regex filter that selects the jet variables used in training, added normalization to inputs

dataset.py
@@ -6,6 +6,7 @@ import torch.utils.data as udata
 import torch
 import pandas as pd
 from tqdm import tqdm
+import awkward as ak
 
 def get_sizes(l, frac=[0.8, 0.1, 0.1]):
     if sum(frac) != 1.0: raise ValueError("Sum of fractions does not equal 1.0")
@@ -15,12 +16,39 @@ def get_sizes(l, frac=[0.8, 0.1, 0.1]):
     val_size = l - train_size - test_size
     return [train_size, test_size, val_size]
 
+def normalize(data):
+    copyData = data.copy(deep=True)
+    for column in copyData.columns:
+        if "jEtaAK8" in column:
+            copyData[column] = abs(copyData[column])
+        elif column in ["METrHT_pt30","dEtaj12AK8","dRJ12AK8","dPhiMinjMETAK8","nnOutput"]:
+            continue
+        elif "dPhijMETAK8" in column:
+            continue
+        elif "jPhiAK8" in column:
+            continue
+        elif "nnOutput" in column:
+            continue
+        else:
+            copyData[column] = np.log(copyData[column])
+    return(copyData)
+        
+
 class RootDataset(udata.Dataset):
-    def __init__(self, path, sigFiles, bkgFiles, eventVar, jetVar):
+    def __init__(self, path, sigFiles, bkgFiles, eventVar, jetVar, numOfJetsToKeep):
         self.eventVar = eventVar
         self.jetVar = jetVar
-        bkgData, bkgNames = self.open_data(path, bkgFiles, 0)
-        sigData, sigNames = self.open_data(path, sigFiles, len(bkgFiles))
+        bkgData, bkgNames = self.open_data(path, bkgFiles, 0, numOfJetsToKeep)
+        sigData, sigNames = self.open_data(path, sigFiles, len(bkgFiles), numOfJetsToKeep)
+        allData = pd.concat([bkgData,sigData])
+        # normalizing inputs
+        inputVars = list(allData.columns)
+        inputVars.remove("label")
+        inputData = allData[inputVars]
+        normedInputData = normalize(inputData)
+        normedData= pd.concat([normedInputData,allData["label"]],axis=1)
+        normedData.replace([np.inf, -np.inf], np.nan, inplace=True)
+        normedData.dropna(inplace=True)
         self.allData = pd.concat([bkgData,sigData])
         self.names = pd.concat([bkgNames, sigNames])
 
@@ -37,7 +65,7 @@ class RootDataset(udata.Dataset):
                 paravalue = float(paravalue)
         return paravalue
 
-    def open_data(self, path, dataFiles, labelShift):
+    def open_data(self, path, dataFiles, labelShift, numOfJetsToKeep):
         vars = None
         names = None
         for i, key in enumerate(dataFiles):
@@ -45,18 +73,22 @@ class RootDataset(udata.Dataset):
             for fileName in fileList:
                 f = up.open(path  + fileName + ".root")
                 print(fileName)
-                var = f["tree"].arrays(self.eventVar + self.jetVar,  library="pd")#.head(100)
-                var = var.filter(regex='[0]|[1]|[2]')
+                eventvar = f["tree"].arrays(self.eventVar,  library="pd")#.head(100)
+                jetvar = f["tree"].arrays(self.jetVar,  library="pd")#.head(100)
+                regex = ""
+                for nj in range(numOfJetsToKeep):
+                    regex += '\['+str(nj)+'\]|'
+                jetvar = jetvar.filter(regex=regex[:-1])
+                var = pd.concat([eventvar , jetvar ],axis=1)
                 var["label"] = [i+labelShift]*len(var)
                 vars = pd.concat([vars, var]) if vars is not None else var
-
+                print("Number of events: {}".format(len(var)))
                 name = pd.DataFrame()
-                name["w"]     = [1.0]*len(var) 
+                name["w"]     = [1.0]*len(var) # replace with the branch for weights
                 name["mMed"]  = [self.getPara(fileName,  "mMed")]*len(var)
                 name["mDark"] = [self.getPara(fileName, "mDark")]*len(var)
                 name["rinv"]  = [self.getPara(fileName,  "rinv")]*len(var)
                 names = pd.concat([names, name]) if names is not None else name
-
         return vars, names
 
     def __len__(self):
@@ -83,19 +115,29 @@ if __name__=="__main__":
     ds = args.dataset
     ft = args.features
 
-    dataset = RootDataset(ds.path, ds.signal, ds.background, ft.eventVariables, ft.jetVariables)
+    dataset = RootDataset(ds.path, ds.signal, ds.background, ft.eventVariables, ft.jetVariables, ft.numOfJetsToKeep)
     loader = udata.DataLoader(dataset=dataset, batch_size=dataset.__len__(), num_workers=0)
+    x = next(iter(loader))
+    print("Data")
+    print(x["data"])
+    print("label")
+    print(x["label"])
+    print("mMed")
+    print(x["mMed"])
+    print("mDark")
+    print(x["mDark"])
+    print("rinv")
+    print(x["rinv"])
+    uniqueLabels, uniqueCounts = np.unique(x["label"],return_counts=True)
+    for i in range(len(uniqueLabels)):
+        uniqueLabel = uniqueLabels[i]
+        uniqueCount = uniqueCounts[i]
+        print(uniqueLabel,uniqueCount)    
+    sizes = get_sizes(len(dataset), ds.sample_fractions)
+    train, val, test = udata.random_split(dataset, sizes, generator=torch.Generator().manual_seed(42))
+    loader = udata.DataLoader(dataset=train, batch_size=train.__len__(), num_workers=0)
+    print("After split")
     x = next(iter(loader))
     print(x["data"])
     print(x["label"])
-    print(x["mMed"])
-    print(x["mDark"])
-    print(x["rinv"])
-
-    #sizes = get_sizes(len(dataset), ds.sample_fractions)
-    #train, val, test = udata.random_split(dataset, sizes, generator=torch.Generator().manual_seed(42))
-    #loader = udata.DataLoader(dataset=train, batch_size=train.__len__(), num_workers=0)
-    #x = next(iter(loader))
-    #print(x["data"])
-    #print(x["label"])
-        
+    print(npdata.shape)